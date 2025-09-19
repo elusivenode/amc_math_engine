072ebab411dd1f7280aaabedf9b33d3e2b9d8161
elusivenode <elusivenode@gmail.com>
2025-09-19 16:38:21 +1000

    prepping for cloudflare

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,8 +1,10 @@
+import adapter from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
   preprocess: [vitePreprocess()],
   kit: {
+    adapter: adapter(),
     alias: {
       $components: 'src/lib/components',
       $lib: 'src/lib'
