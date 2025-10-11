58f161b9a124eff2f285e3fb7499ae51bf929572
elusivenode <elusivenode@gmail.com>
2025-10-11 13:44:57 +1000

    refining message after alg basic completed.

--- frontend/src/lib/journey/messages/algebra-basic-to-intermediate.ts
+++ frontend/src/lib/journey/messages/algebra-basic-to-intermediate.ts
@@ -1,7 +1,9 @@
 import type { JourneyMessageConfig } from './types';
 
 const message: JourneyMessageConfig = {
-  message: 'Well done.',
+  message: `Excellent work.
+Working through those 15 problems has stress-tested your basic algebra and fine-tuned your problem interpretation.
+Journey forward with vigilence.  The Order of the Olympiad needs you.  Reason cannot fall to bureaucracy`,
 };
 
 export default message;
