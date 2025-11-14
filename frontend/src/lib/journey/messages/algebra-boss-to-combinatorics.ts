ed9c2ba93e3e63b9e475d9c1548be7a677e97857
elusivenode <elusivenode@gmail.com>
2025-11-15 09:26:56 +1000

    transition alg to comb

---
+++ frontend/src/lib/journey/messages/algebra-boss-to-combinatorics.ts
@@ -0,0 +1,11 @@
+import type { JourneyMessageConfig } from './types';
+
+const algebraBossToCombinatorics: JourneyMessageConfig = {
+  heading: 'Algebra Avengers',
+  message:
+    'And with last effort, you have cracked the door to combinatorics ajar. Congratulations on completing the Algebraic Avengers path.\n\n' +
+    'You have a strong algebraic toolkit on which to build. Learning never stops.',
+  ctaLabel: 'Enter Combinatoric Crusaders',
+};
+
+export default algebraBossToCombinatorics;
